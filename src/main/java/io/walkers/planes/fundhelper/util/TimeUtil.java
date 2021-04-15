ccc663eb89b6380bc212241543874744a0dad104
Planeswalker23 <fanyidongdong@gmail.com>
2021-04-15 11:11:20 +0800

    Refactor: TimeUtil Provider new function(String to java.util.Date)

--- src/main/java/io/walkers/planes/fundhelper/util/TimeUtil.java
+++ src/main/java/io/walkers/planes/fundhelper/util/TimeUtil.java
@@ -15,11 +15,20 @@ public class TimeUtil {
     private static ThreadLocal<DateFormat> SQL_DATE_FORMAT_THREAD_LOCAL = ThreadLocal.withInitial(() -> new SimpleDateFormat(TimeFormatDict.FORMAT_YYYY_MM_DD));
 
     /**
-     * 时间格式化 String to java.sql.Date
+     * 时间格式化 String to {@link java.sql.Date}
      * @param timeString 时间入参
      * @return java.sql.Date
      */
-    public static java.sql.Date string2Date(String timeString) {
+    public static java.sql.Date string2JavSqlDate(String timeString) {
+        return new java.sql.Date(TimeUtil.string2JavaUtilDate(timeString).getTime());
+    }
+
+    /**
+     * 时间格式化 String to {@link java.util.Date}
+     * @param timeString 时间入参
+     * @return java.util.Date
+     */
+    public static java.util.Date string2JavaUtilDate(String timeString) {
         java.util.Date time;
         try {
             // 取字符串前10位
@@ -28,6 +37,6 @@ public class TimeUtil {
             log.error("Date format failed, source parameter is: {}, caused by: ", timeString, e);
             throw new RuntimeException("Date format failed");
         }
-        return new java.sql.Date(time.getTime());
+        return time;
     }
 }
